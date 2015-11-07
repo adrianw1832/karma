c40cbe97698494b78c362f0fc0ca552b3d54820d
Adrian Wong <adrianwong1832@gmail.com>
2015-11-07 17:10:46 +0000

    Add restriction so users have to be signed in to create posts

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 feature 'Posts feature' do
+  let(:user) { create(:user) }
+
   context 'no posts have been created' do
     scenario 'should prompt the use to do add new posts' do
       visit posts_path
@@ -11,6 +13,7 @@ feature 'Posts feature' do
 
   context 'posts have been created' do
     let!(:post) { create(:post) }
+
     scenario 'should be able to see it on the page' do
       visit posts_path
       expect(page).to have_content 'I need a lift'
@@ -20,7 +23,8 @@ feature 'Posts feature' do
 
   context 'creating posts' do
     context 'user is logged in' do
-      scenario 'users can create a new post' do
+      scenario 'user can create a new post' do
+        sign_in_as(user)
         visit root_path
         click_link 'Add a new post'
         fill_in 'Title', with: 'I need a lift'
@@ -29,6 +33,16 @@ feature 'Posts feature' do
         expect(current_path).to eq posts_path
         expect(page).to have_content 'I need a lift'
         expect(page).to have_content 'I need to go to the supermarket'
+      end
+    end
+
+    context 'user is not logged in' do
+      scenario 'user cannot create a new post and is redirected' do
+        visit root_path
+        click_link 'Add a new post'
+        expect(current_path).to eq new_user_session_path
+        expect(page).to have_content 'Email'
+        expect(page).to have_content 'Password'
       end
     end
   end
